f0b47fcba09cfea72f907145e33004f7ab5e31a0
Devin <devin.everitt96@gmail.com>
2024-01-02 20:18:56 -0500

    Made the boxes save the text after much headache

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,6 @@ var today = dayjs().format('MMMM, d, YYYY');
 
 var time = dayjs().hour()
 
-console.log(time);
 
 $('#currentDay').text(today);
 
@@ -148,23 +147,27 @@ $(document).ready(function () {
 });
 
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+$(document).ready(function() {
+  $('.description').each(function() {
+    var key = `textareaValue_${$(this).parent().attr('id')}`;
+    var savedValue = localStorage.getItem(key);
+    
+    if (savedValue) {
+           var textareaValue = JSON.parse(savedValue);
+      
+      $(this).val(textareaValue);
+    }
+  });
 });
+
+
+$('.saveBtn').on('click', function() {
+  var textarea = $(this).prev('.description');
+  var textareaValue = textarea.val();
+  var textValue = JSON.stringify(textareaValue);
+  var key = `textareaValue_${textarea.parent().attr('id')}`;
+  
+    localStorage.setItem(key, textValue);
+});
+
